048087de6d9602369c38dc79b3a5c38d734a29bd
Iwak <157107911+KuntilBogel@users.noreply.github.com>
2024-09-06 15:25:57 +0000

    yeah

--- scraper/chars_scrape.js
+++ scraper/chars_scrape.js
@@ -2,27 +2,49 @@ import { writeFile } from 'fs/promises';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 
-// Sample data to be written to the test.json file
-const data = {
-    name: "Test Name",
-    description: "This is a test JSON file",
-    version: "1.0.0"
-};
+import axios from 'axios';
+const m = []
 
-// Convert the JavaScript object into a JSON string
-const jsonData = JSON.stringify(data, null, 2);
+for (let i = 0; i < 3; i++) {
+    console.log("getting " + (i+1))
+    const response = await axios.post(
+        'https://graphql.anilist.co/',
+        {
+            'query': '\n    {\n      Page(page:' + (i + 1) + ', perPage: 500){\n        media (sort:POPULARITY_DESC, isAdult: false){      \n          title {\n            english\n           \tromaji            \n          }\n          id\n          characters(sort:FAVOURITES_DESC, perPage: 5){\n            nodes{\n              id\n              name {\n                full\n              }\n              image{\n                large\n              }\n            }\n          }      \n        }\t\n      }\n  }',
+            'variables': {}
+        },
+        {
+            headers: {
+                'accept': '/',
+                'accept-language': 'en-US,en;q=0.9',
+                'cache-control': 'no-cache',
+                'content-type': 'application/json',
+                'pragma': 'no-cache',
+                'priority': 'u=1, i',
+                'sec-ch-ua': '"Chromium";v="128", "Not;A=Brand";v="24", "Google Chrome";v="128"',
+                'sec-ch-ua-mobile': '?0',
+                'sec-ch-ua-platform': '"Windows"',
+                'sec-fetch-dest': 'empty',
+                'sec-fetch-mode': 'cors',
+                'sec-fetch-site': 'cross-site',
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36',
+            }
+        }
+    );
+    m.push(...response.data.data.Page.media)
 
-// Get the current directory name (__dirname equivalent in ESM)
+}
+const jsonData = JSON.stringify(m, null, 2);
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-// Resolve the path to ../test.json (parent directory of current file)
-const filePath = join(__dirname, '../test.json');
+const filePath = join(__dirname, '../animechars.json');
 
-// Write the JSON string to the file
 try {
     await writeFile(filePath, jsonData);
-    console.log('Successfully wrote to ../test.json');
+    console.log('Successfully wrote to ../animechars.json');
 } catch (err) {
     console.error('Error writing to file', err);
+    process.exit(1)
 }
